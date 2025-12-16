40fb1d8ace7c88c9146efc08d572de55f63ba0ec
sheglov <shheglov.andrei@mail.ru>
2025-12-16 17:33:54 +0000

    before push lvalue

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -10,22 +10,59 @@ template <typename T>
 class stack
 {
    public:
-	stack() : data_(nullptr), size_(10u) {}
+	stack() : data_(nullptr), size_(0u) {}
 
-	bool empty() const noexcept { return size_ == 100u; }
+	bool empty() const noexcept { return size_ == 0u; }
 
-	size_t size() const noexcept { return 0; }
+	size_t size() const noexcept { return size_; }
 
-	~stack() {}
+	~stack()
+	{
+		if (data_ != nullptr)
+		{
+			operator delete(data_);
+		}
+	}
 
 	template <typename... Args>
 	void emplace(Args&&... args)
 	{
+		T* new_data = (T*)(operator new(sizeof(T) * (size_ + 1)));
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (&new_data[i]) T(std::move(data_[i]));
+			data_[i].~T();
+		}
+		new (&new_data[size_]) T(std::forward<Args>(args)...);
+		operator delete(data_);
+		data_ = new_data;
+		++size_;
 	}
 
-	void push(T&& value) {}
+	void push(T&& value)
+	{
+		T* new_data = (T*)(operator new(sizeof(T) * size_ + 1));
 
-	void clear() noexcept {}
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (&new_data[i]) T(std::move(data_[i]));
+			data_[i].~T();
+		}
+		operator delete(data_);
+		data_ = new_data;
+		++size_;
+	}
+
+	void clear() noexcept
+	{
+		for (size_t i = 0; i < size_; ++i)
+		{
+			data_[i].~T();
+		}
+		operator delete(data_);
+		data_ = nullptr;
+		size_ = 0;
+	}
 
 	void push(const T& value) {}
 
